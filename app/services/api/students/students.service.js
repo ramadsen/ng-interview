ebb5fc8166505b78bd5598bd5241684f51899d30
Becky Davis <becky.rad@gmail.com>
2018-02-12 23:56:06 -0700

    initial commit for displaying student data

--- app/services/api/students/students.service.js
+++ app/services/api/students/students.service.js
@@ -5,21 +5,56 @@
 		.module('ngInterview.api.students')
 		.service('StudentsService', StudentsService);
 
-	StudentsService.$inject = [];
-	function StudentsService() {
+	StudentsService.$inject = ['$http', '$q'];
+	function StudentsService($http, $q) {
 
 		/**
 		 * Exposed functions
 		 */
+		var service = {
+			getStudents: getStudents
+		}
+		return service;
 
-		this.getName = getName; // This function serves no purpose. It's just here as an example.
+		////////////		 
+
+		// this.getStudents = getStudents; // This function serves no purpose. It's just here as an example.
 
 		/**
 		 * Implementations
 		 */
 
-		function getName() {
-			return 'studentsService';
+		function getStudents() {
+		    var req = {
+		      method: 'GET',
+		      url: 'http://il-resume-api.azurewebsites.net/api/students',
+		      timeout: '30000'
+		    }
+		    var defer = $q.defer();
+		    $http(req).success(function (data, status, headers, config) {
+		      try{
+		      	if (status == 503) {
+		      		defer.reject("Student service down");
+		      	}
+		      	//since the data is good if it's an array, I'm just checking for that.
+		      	//I would do other checks if it could come back as other stuff or malformed in some other way.
+		      	else if (Object.prototype.toString.call( data ) != '[object Array]') {
+		      		defer.reject("Malformed data");
+		      	} else {
+		      		for (var i = 0; i < data.length; i++) {
+		      			data[i].Name = data[i].FirstName + ' ' + data[i].LastName;
+		      			data[i].showData = false;
+		      		}
+		      		defer.resolve(data);
+		      	}
+		      }
+		      catch(e){
+		        defer.reject("Malformed data");
+		      }
+		    }).error(function (data, status, headers, config) {
+		      defer.reject(status);
+		    });
+		    return defer.promise;
 		}
 	}
 })();
--- app/students/students.controller.js
+++ app/students/students.controller.js
@@ -5,14 +5,15 @@
 		.module('ngInterview.students')
 		.controller('StudentsController', StudentsController);
 
-	StudentsController.$inject = [];
-	function StudentsController() {
+	StudentsController.$inject = ['StudentsService'];
+	function StudentsController(StudentsService) {
 
 		/**
 		 * Model
 		 */
 
 		var vm = this;
+		var requestCounter = 0;
 
 		/**
 		 * Initialization
@@ -26,6 +27,29 @@
 
 		function activate() {
 			// Initialization code goes here
+			loadStudents();
+
+		}
+
+		function loadStudents() {
+			requestCounter++;
+		    var getStudents = StudentsService.getStudents();
+		    getStudents.then(function (data) {
+		    	vm.state = "good-data";
+		    	vm.students = data;
+
+		    }).catch(function(e){
+		    	// debugger;
+		    	console.error(e || "Request failed");
+		    	//limiting the number of times it tries to avoid infinite loop
+		    	if (e == 503 && requestCounter <= 5) {
+		    		loadStudents();
+		    	} else if (requestCounter >= 6) {
+		    		vm.state = "service-down";
+		    	} else {
+		    		vm.state = "bad-data";
+		    	}
+		    });
 		}
 	}
 })();
